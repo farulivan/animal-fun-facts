5803171f495fa9dda1b57fc7065f0db339ae7868
Farul Ivan <farulivan@gmail.com>
2022-10-03 15:10:58 +0700

    adding all static element

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
+import { animals } from './animals'
 import './App.css';
 
 function App() {
+  const displayFact = (e) => {
+    // const selectedAnimal = e.target.alt
+    // const optionIndex = Math.floor(Math.random() * selectedAnimal.facts.length)
+    // const randomFacts = selectedAnimal.facts[optionsIndex]
+    // document.getElementById('fact').innerHTML = randomFacts
+  }
+  
+  const title = ''
+  const background = (
+    <img className='background' alt='ocean' src='/images/ocean.jpg' />
+  )
+  const images = []
+  for(const animal in animals){
+    images.push(
+      <img 
+        key={animal}
+        className='animal'
+        alt={animal}
+        src={animals[animal].image}
+        aria-label={animal}
+        role='button'
+        onClick={displayFact}
+      />
+    )
+  }
+  const animalFacts = (
+    <div>
+      <h1>
+        {title === '' && 'Click an animal for a fun fact'}
+      </h1>
+      {background}
+      <div className='animals'>
+        {images}
+      </div>
+      <p id='fact'></p>
+    </div>
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {animalFacts}
+      <img />
     </div>
   );
 }
