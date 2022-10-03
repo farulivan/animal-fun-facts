96293df3818949389db4f4d1064cc526329c1e72
Farul Ivan <farulivan@gmail.com>
2022-10-03 15:24:02 +0700

    finishing the project with some conditional syntax

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
 import { animals } from './animals'
 import './App.css';
 
 function App() {
   const displayFact = (e) => {
-    // const selectedAnimal = e.target.alt
-    // const optionIndex = Math.floor(Math.random() * selectedAnimal.facts.length)
-    // const randomFacts = selectedAnimal.facts[optionsIndex]
-    // document.getElementById('fact').innerHTML = randomFacts
+    const selectedAnimal = e.target.alt
+    const optionIndex = Math.floor(Math.random() * animals[selectedAnimal].facts.length)
+    const randomFacts = animals[selectedAnimal].facts[optionIndex]
+    document.getElementById('fact').innerHTML = randomFacts
   }
   
   const title = ''
   const background = (
     <img className='background' alt='ocean' src='/images/ocean.jpg' />
   )
+  const showBackground = true
   const images = []
+  
   for(const animal in animals){
     images.push(
       <img 
@@ -28,22 +29,23 @@ function App() {
       />
     )
   }
+  
   const animalFacts = (
     <div>
       <h1>
-        {title === '' && 'Click an animal for a fun fact'}
+        {title || 'Click an animal for a fun fact'}
       </h1>
-      {background}
+      {showBackground && background}
       <div className='animals'>
         {images}
       </div>
       <p id='fact'></p>
     </div>
   )
+
   return (
     <div className="App">
       {animalFacts}
-      <img />
     </div>
   );
 }
